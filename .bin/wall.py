52f1925b6b1e855694db2dc0479403aa2788f98a
Kerolos Zaki <52022280+ker0olos@users.noreply.github.com>
2021-03-22 02:00:01 +0200

    refactored the wallpaper scripts to allow more readable output whlie you wait for your next cute waifu

--- .bin/wall.py
+++ .bin/wall.py
@@ -26,6 +26,7 @@ from os.path import expanduser
 import sys
 import requests
 import urllib
+import subprocess
 from PIL import ImageFile
 
 # ---------------------
@@ -34,103 +35,79 @@ from PIL import ImageFile
 
 # Returns false on status code error
 def validURL(URL):
-    statusCode = requests.get(URL, headers = {'User-agent':'getWallpapers'}).status_code
-    if statusCode == 404:
-        return False
-    else: return True
+  statusCode = requests.get(URL, headers = {'User-agent':'getWallpapers'}).status_code
+  if statusCode == 404:
+    return False
+  else: return True
 
 # Returns false if subreddit doesn't exist
 def verifySubreddit(subreddit):
-    URL = 'https://reddit.com/r/{}.json'.format(subreddit)
-    result= requests.get(URL, headers = {'User-agent':'getWallpapers'}).json()
-    try:
-        result['error']
-        return False
-    except:
-        return True
+  URL = 'https://reddit.com/r/{}.json'.format(subreddit)
+  result= requests.get(URL, headers = {'User-agent':'getWallpapers'}).json()
+  try:
+    result['error']
+    return False
+  except:
+    return True
 
 # Returns list of posts from subreddit as json
 def getPosts(subreddit, after):
-    allPosts = []
+  allPosts = []
     
-    # t	= one of (hour, day, week, month, year, all)
-    # URL = 'https://reddit.com/r/{}/top/.json?t=all&limit={}&after={}'.format(subreddit, limit, after)
-    URL = 'https://reddit.com/r/{}/random/.json?t=day&limit={}&after={}'.format(subreddit, limit, after)
+  # https://www.reddit.com/dev/api/#GET_hot
+  URL = 'https://reddit.com/r/{}/hot/.json?&limit={}&after={}'.format(subreddit, limit, after)
 
-    posts = requests.get(URL, headers = {'User-agent':'getWallpapers'}).json()
-    # allPosts.append(posts['data']['children'])
-
-    for post in posts['data']['children']:
-        allPosts.append(post)
-    after = posts['data']['after']
-    
-    return allPosts
+  posts = requests.get(URL, headers = {'User-agent':'getWallpapers'}).json()
+  for post in posts['data']['children']:
+    allPosts.append(post)
+  after = posts['data']['after']
+  return allPosts
 
 # Returns false if URL is not an image
 def isImg(URL):
-    if URL.endswith(('.png', '.jpeg', '.jpg')):
-        return True
-    else: return False
+  if URL.endswith(('.png', '.jpeg', '.jpg')):
+    return True
+  else: return False
 
 # Returns false if image from URL is not HD (Specified by min-/max_width)
-def isHD(URL, min_width, min_height):
-    file = urllib.request.urlopen(URL)
-    size = file.headers.get("content-length")
-    if size: size = int(size)
-    p = ImageFile.Parser()
-    while 1:
-        data = file.read(1024)
-        if not data:
-            break
-        p.feed(data)
-        if p.image:
-            # return p.image.size
-            if p.image.size[0] >= min_width and p.image.size[1] >= min_height:
-                return True
-            else:
-                return False
-    file.close()
-    return False
-
-# Returns false if image from URL is not landscape
-def isLandscape(URL):
-    file = urllib.request.urlopen(URL)
-    size = file.headers.get("content-length")
-    if size: size = int(size)
-    p = ImageFile.Parser()
-    while 1:
-        data = file.read(1024)
-        if not data:
-            break
-        p.feed(data)
-        if p.image:
-            # return p.image.size
-            if p.image.size[0] >= p.image.size[1]:
-                return True
-            else:
-                return False
-    file.close()
-    return False
+def isFit(URL, min_width, min_height):
+  file = urllib.request.urlopen(URL)
+  size = file.headers.get("content-length")
+  if size: size = int(size)
+  p = ImageFile.Parser()
+  while 1:
+    data = file.read(1024)
+    if not data:
+      break
+    p.feed(data)
+    if p.image:
+      # return p.image.size
+      if p.image.size[0] >= p.image.size[1] and p.image.size[0] >= min_width and p.image.size[1] >= min_height:
+        return True
+      return False
+  file.close()
+  return False
 
 # Returns true if image from URL is already downloaded
 def alreadyDownloaded(URL):
-    imgName = os.path.basename(URL)
-    localFilePath = os.path.join(directory, imgName)
-    if(os.path.isfile(localFilePath)):
-        return True
-    else: return False
+  imgName = os.path.basename(URL)
+  localFilePath = os.path.join(directory, imgName)
+  if(os.path.isfile(localFilePath)):
+    return True
+  else: return False
 
 # Returns false if image from post/URL is not from reddit or imgur domain
 def knownURL(post):
-    if post.lower().startswith('https://i.redd.it/') or post.lower().startswith('http://i.redd.it/') or post.lower().startswith('https://i.imgur.com/') or post.lower().startswith('http://i.imgur.com/'):
-        return True
-    else: return False
+  if post.lower().startswith('https://i.redd.it/') or post.lower().startswith('http://i.redd.it/') or post.lower().startswith('https://i.imgur.com/') or post.lower().startswith('http://i.imgur.com/'):
+    return True
+  else: return False
 
 # Returns true if image from post/URL is stored locally
 def storeImg(post):
-    if urllib.request.urlretrieve(post, os.path.join(directory, os.path.basename(post))):
-        return True
-    else: return False
+  if urllib.request.urlretrieve(post, os.path.join(directory, os.path.basename(post))):
+    return True
+  else:
+    return False
 
 # ---------------------
 # START SCRIPT --------
@@ -138,16 +115,22 @@ def storeImg(post):
 
 # Check if subreddit name is specified as parameter
 try:
-    subreddit = sys.argv[1]
+  subreddit = sys.argv[1]
 except:
-    pass
+  pass
 
 directory = expanduser(directory)
 
 # Exits if invalid subreddit name
 if not verifySubreddit(subreddit):
-    print('r/{} is not a valid subreddit'.format(subreddit))
-    sys.exit()
+  print('r/{} is not a valid subreddit'.format(subreddit))
+  sys.exit()
+
+# Print starting message
+
+print('\n--------------------------------------------')
+print('r/{} ({}x{})'.format(subreddit, str(min_width), str(min_height)))
+print('--------------------------------------------\n')
 
 # For reddit pagination (Leave empty)
 after = ''
@@ -161,70 +144,61 @@ index = 1
 # Counting amount of images downloaded
 downloadCount = 0
 
-# Print starting message
-# print()
-# print('--------------------------------------------')
-# print('Downloading to      : ' + directory)
-# print('From r/             : ' + subreddit)
-# print('Minimum resolution  : ' + str(min_width) + 'x' + str(min_height))
-# print('--------------------------------------------')
-# print()
-
 # Loops through all posts
 for post in posts:
 
-    # Skip post with less than 10 up votes
-    if post['data']['ups'] < 10:
+  # print the post's title
+  print(post['data']['title'])
+
+  # Skip post with less than 10 up votes
+  if post['data']['ups'] < 10:
+    print('  Skipping: not enough up votes')
+    index += 1
+    continue
+  
+  # Shortening variable name
+  post = post['data']['url']
+
+  # Skip post on 404 error
+  if not validURL(post):
+    print('  Skipping: invalid url')
+    index += 1
+    continue
+
+  # Skip unknown URLs
+  elif not knownURL(post):
+    print('  Skipping: unhandled url')
+    index += 1
+    continue
+
+  # Skip post if not image
+  elif not isImg(post):
+    print('  Skipping: no image in this post')
+    index += 1
+    continue
+
+  # Skip already downloaded images
+  elif alreadyDownloaded(post):
+    print('  Skipping: already used image')
+    index += 1
+    continue
+
+  # Skip post should be HD and landscape
+  elif not isFit(post, min_width, min_height):
+    print('  Skipping: low resolution or portrait image')
+    index += 1
+    continue
+
+  # All checks cleared, download image
+  else:
+    # Store image from post locally
+    if storeImg(post):
+      # update current wallpaper with the new one
+      subprocess.Popen(['wall.sh', '-u', os.path.join(directory, os.path.basename(post))])
       index += 1
-      continue
-    
-    # Shortening variable name
-    post = post['data']['url']
-
-    # Skip post on 404 error
-    if not validURL(post):
-        # print('{}) 404 error'.format(index))
-        index += 1
-        continue
-
-    # Skip unknown URLs
-    elif not knownURL(post):
-        # print('{}) Skipping unknown URL'.format(index))
-        index += 1
-        continue
-
-    # Skip post if not image
-    elif not isImg(post):
-        # print('{}) No image in this post'.format(index))
-        index += 1
-        continue
-
-    # Skip post if not landscape
-    elif not isLandscape(post):
-        # print('{}) Skipping portrait image'.format(index))
-        index += 1
-        continue
-    
-    # Skip post if not HD
-    elif not isHD(post, min_width, min_height):
-        # print('{}) Skipping low resolution image'.format(index))
-        index += 1
-        continue
-
-    # Skip already downloaded images
-    elif alreadyDownloaded(post):
-        index += 1
-        continue
-
-    # All checks cleared, download image
+      downloadCount += 1
+      break
+    # For unexpected errors
     else:
-        # Store image from post locally
-        if storeImg(post):
-            index += 1
-            downloadCount += 1
-            print(os.path.join(directory, os.path.basename(post)))
-            break
-        # For unexpected errors
-        else:
-            # print('Unexpected error')
-            index += 1
+      print('  Skipping: unexpected error')
+      index += 1
--- .bin/wall.sh
+++ .bin/wall.sh
@@ -2,33 +2,23 @@
 
 DEFAULT_NAME="$HOME/Pictures/Wall/default.jpg"
 
-# set previous wall as placeholder
-feh --bg-fill "$DEFAULT_NAME"
-
 if [[ "$1" == "-u" ]]; then
-  # Using Reddit API
-
-  [ ! -d "$HOME/Pictures/Wall" ] && mkdir "$HOME/Pictures/Wall"
-
-  # UwU <3
-  FILE_NAME=$(wall.py Animewallpaper)
-
-  # Set the picture as wallpaper
-
   # create copies to the new wallpaper file
-  cp "$FILE_NAME" "$DEFAULT_NAME" -f
+  cp "$2" "$DEFAULT_NAME" -f
 
   # set the picture as the current active wallpaper
-  feh --bg-fill "$FILE_NAME"
+  feh --bg-fill "$DEFAULT_NAME"
 
   # update notification
 
-  ACTION=$(dunstify -i "$FILE_NAME" "Your wallpaper" "<b>Just got updated</b>" -u low --action="default,Open")
+  ACTION=$(dunstify -i "$DEFAULT_NAME" "Your wallpaper" "<b>Just got updated</b>" -u low --action="default,Open")
 
   # when clicked open the wallpaper in default app
   case "$ACTION" in
   "default")
-    xdg-open "$FILE_NAME"
+    xdg-open "$DEFAULT_NAME"
     ;;
   esac
+else
+  feh --bg-fill "$DEFAULT_NAME"
 fi
