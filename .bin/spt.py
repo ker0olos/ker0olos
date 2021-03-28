6c58707e6653186abf0f723ade1cee0df43bb30c
Kerolos Zaki <52022280+ker0olos@users.noreply.github.com>
2021-03-29 02:54:58 +0300

    spt.py now plays pre-defined playlists when nothing is queued so I never have to ever open the spotify app again

--- .bin/spt.py
+++ .bin/spt.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import time
+import random
 import subprocess
 
 import http.client as httplib
@@ -29,29 +30,18 @@ except:
 spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ['SPOTIFY_CLIENT_ID'],
                                                client_secret=os.environ['SPOTIFY_CLIENT_SECRET'],
                                                redirect_uri='http://localhost:8888/callback',
-                                               cache_path=os.environ['HOME'] + '/.spotify_cache',
+                                               cache_path=os.environ['HOME']+'/.spotify_cache',
                                                scope='user-read-playback-state,user-modify-playback-state,user-library-read,user-library-modify,user-read-recently-played'))
 
-active=True
 currently_playing=spotify.currently_playing()
 
 # nothing is playing
 # output info about last recently played track
 if (not currently_playing):
-  active=False
-  currently_playing={
-    'is_playing': False,
-    'item': spotify.current_user_recently_played(limit=1)['items'][0]['track']
-  }
-
-tid=currently_playing['item']['id']
-
-if (command=='title'):
-  print(currently_playing['item']['name'])
-elif (command=='toggle'):
-  if currently_playing['is_playing']:
-    spotify.pause_playback()
-  elif not active:
+  if (command=='title'):
+    print('Nothing is queued.')
+    # print(spotify.current_user_recently_played(limit=1)['items'][0]['track']['name'])
+  elif (command=='toggle'):
     devices=spotify.devices()['devices']
 
     # try spawning spotifyd if there's working devices
@@ -64,21 +54,48 @@ elif (command=='toggle'):
       print('No devices available.')
       sys.exit()
 
-    # last streamed 40 tracks
-    uris=map(lambda item: item['track']['uri'], spotify.current_user_recently_played(limit=40)['items'])
-    spotify.start_playback(device_id=devices[0]['id'],uris=list(dict.fromkeys(uris)))
+    # my favorite playlists
+    uris=map(lambda item: item['track']['uri'], spotify.playlist_items(random.choice([
+      'spotify:playlist:37i9dQZF1E35tuHOcRhgZZ',
+      'spotify:playlist:37i9dQZF1E3855z4z5OsPs',
+      'spotify:playlist:37i9dQZF1E39jZuNJB5bh7',
+      'spotify:playlist:37i9dQZF1E36Hq1lYk3e5n',
+      'spotify:playlist:37i9dQZF1E39KFXWlmzgOy', 
+      'spotify:playlist:37i9dQZF1E35TMnPZZpmYQ',
+      'spotify:playlist:37i9dQZF1DWV4t1PmvRVd9',
+      'spotify:playlist:37i9dQZF1DWUQM3rmTXpBR'
+    ]))['items']) # https://open.spotify.com/genre/made-for-x-hub
+    
+    # get last streamed 40 tracks (little buggy for whatever fucking reason, I don't care anymore.)
+    # uris=dict.fromkeys(map(lambda item: item['track']['uri'], spotify.current_user_recently_played(limit=40)['items']))
+
+    spotify.start_playback(device_id=devices[0]['id'],uris=list(uris))
+
+    # this is my personal preference
+    spotify.shuffle(True)
+    spotify.repeat('context')
   else:
-    spotify.start_playback()
-elif (command=='skip'):
-  spotify.next_track()
-# elif (command=='previous'):
-#   spotify.previous_track()
-# elif (command=='shuffle'):
-#   spotify.shuffle(not spotify.current_playback()['shuffle_state'])
-elif (command=='state'):
-  print('󰋑') if spotify.current_user_saved_tracks_contains(tracks=[tid])[0] else print('󰋕')
-elif (command=='like'):
-  if not spotify.current_user_saved_tracks_contains(tracks=[tid])[0]:
-    spotify.current_user_saved_tracks_add(tracks=[tid])
-  else:
-    spotify.current_user_saved_tracks_delete(tracks=[tid])
+    print('')
+else:
+  tid=currently_playing['item']['id']
+
+  if (command=='title'):
+    print(currently_playing['item']['name'])
+  elif (command=='toggle'):
+    if currently_playing['is_playing']:
+      spotify.pause_playback()
+    else:
+      spotify.start_playback()
+  elif (command=='skip'):
+    spotify.next_track()
+  # elif (command=='previous'):
+  #   spotify.previous_track()
+  # elif (command=='shuffle'):
+  #   spotify.shuffle(not spotify.current_playback()['shuffle_state'])
+  elif (command=='state'):
+    print('󰋑') if spotify.current_user_saved_tracks_contains(tracks=[tid])[0] else print('󰋕')
+  elif (command=='like'):
+    if not spotify.current_user_saved_tracks_contains(tracks=[tid])[0]:
+      spotify.current_user_saved_tracks_add(tracks=[tid])
+    else:
+      spotify.current_user_saved_tracks_delete(tracks=[tid])
