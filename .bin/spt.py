bb243a6746f800674871082737229b54fa41e7ac
Kerolos Zaki <ker0olos@pm.me>
2021-05-27 12:11:19 +0200

    small improvments to spt.py

--- .bin/spt.py
+++ .bin/spt.py
@@ -22,55 +22,52 @@ try:
   connection.request('HEAD', '/')
   connection.close()
 except:
-  print('No internet connection.') if command=='title' else print('')
+  print('No internet connection') if command=='title' else print('')
   connection.close()
   sys.exit()
 
-spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ['SPOTIFY_CLIENT_ID'],
-                                               client_secret=os.environ['SPOTIFY_CLIENT_SECRET'],
+spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id='9f40f49585234c6d8762592b05ad615a',
+                                               client_secret='660c830473474d95b23996e4cf3ff5a6',
                                                redirect_uri='http://localhost:8888/callback',
                                                cache_path=os.environ['HOME']+'/.spotify_cache',
                                                scope='user-read-playback-state,user-modify-playback-state,user-library-read,user-library-modify,user-read-recently-played'))
 
 currently_playing=spotify.currently_playing()
 
-# my favorite playlists
-# https://open.spotify.com/genre/made-for-x-hub
 playlists = [
-  'spotify:playlist:37i9dQZF1E35tuHOcRhgZZ', # Daily Mix 1
-  'spotify:playlist:37i9dQZF1E3855z4z5OsPs', # Daily Mix 2
-  'spotify:playlist:37i9dQZF1E39jZuNJB5bh7', # Daily Mix 3
-  'spotify:playlist:37i9dQZF1E36Hq1lYk3e5n', # Daily Mix 4
-  # 'spotify:playlist:37i9dQZF1E39KFXWlmzgOy',# Daily Mix 5
-  # 'spotify:playlist:37i9dQZF1E35TMnPZZpmYQ', # Daily Mix 6
-  'spotify:playlist:37i9dQZF1DWV4t1PmvRVd9', # Egyptian Indie
-  # 'spotify:playlist:37i9dQZF1DWUQM3rmTXpBR', # Arab Indie
-  'spotify:playlist:37i9dQZEVXbsA9iS8lxmLk' # Release Radar
+  # https://open.spotify.com/genre/made-for-x-hub
+  [ 'spotify:playlist:37i9dQZF1E35tuHOcRhgZZ', 'Daily Mix 1' ],
+  [ 'spotify:playlist:37i9dQZF1E3855z4z5OsPs', 'Daily Mix 2' ],
+  [ 'spotify:playlist:37i9dQZF1E39jZuNJB5bh7', 'Daily Mix 3' ],
+  [ 'spotify:playlist:37i9dQZF1E36Hq1lYk3e5n', 'Daily Mix 4' ],
+  [ 'spotify:playlist:37i9dQZEVXbsA9iS8lxmLk', 'Release Radar' ],
+  # [ 'spotify:playlist:37i9dQZF1DWV4t1PmvRVd9', 'Egyptian Indie' ],
+  # [ 'spotify:playlist:37i9dQZF1DWUQM3rmTXpBR', 'Arab Indie' ],
+  [ 'spotify:artist:1PhE6rv0146ZTQosoPDjk8', 'Calliope Mori' ]
 ]
 
 # nothing is playing
-if not currently_playing or command == 'skip':
+if not currently_playing or command == 'skip' or command == 'play':
   id = math.ceil(int(datetime.datetime.utcnow().strftime('%S')) / 10) - 1
 
-  if command == 'title':
-    # print('Nothing is queued.')
-    print('It\'s time for ' + spotify.playlist(playlists[id])['name'])
-    # print(spotify.current_user_recently_played(limit=1)['items'][0]['track']['name'])
-  elif command == 'toggle' or command == 'skip':
+  if command == 'title' and 'artist' in playlists[id][0]:
+    print('I can hear {}!!'.format(playlists[id][1]))
+  elif command == 'title':
+    print('Time for your {}.'.format(playlists[id][1]))
+  elif command == 'toggle' or command == 'skip' or command == 'play':
     devices=spotify.devices()['devices']
 
     if len(devices) == 0:
       print('No active devices found.')
       sys.exit()
 
-    spotify.start_playback(device_id=devices[0]['id'],context_uri=playlists[id])
+    if (command == 'play'):
+      for i, item in enumerate(playlists):
+        print('{}) {}'.format(i, item[1]))
+      id = int(input('Make your choice: '))
+    
+    spotify.start_playback(device_id=devices[0]['id'],context_uri=playlists[id][0])
 
-    # get last streamed 40 tracks (little buggy for whatever fucking reason, I don't care anymore.)
-    # uris=map(lambda item: item['track']['uri'], spotify.current_user_recently_played(limit=40)['items'])
-
-    # spotify.start_playback(device_id=devices[0]['id'],uris=list(dict.fromkeys(uris)))
-
-    # this is my personal preference
     spotify.shuffle(True)
     spotify.repeat('context')
   else:
