b8ffe822b6273d19410f28cdb4b16bc29f5fd6ad
Kerolos Zaki <ker0olos@pm.me>
2021-09-08 16:50:55 +0200

    removed or commented unused bits

--- .bin/vaudio.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-MICROPHONE="alsa_input.usb-0c8f_USB_Audio_Device-00.mono-fallback"
-SPEAKERS="bluez_sink.7B_F8_19_1B_EC_E1.a2dp_sink"
-#SPEAKERS="alsa_output.pci-0000_00_1b.0.analog-stereo"
-
-pactl load-module module-null-sink sink_name=Virtual1
-pactl load-module module-loopback source=$MICROPHONE sink=Virtual1
-pactl load-module module-loopback source=Virtual1.monitor sink=$SPEAKERS
