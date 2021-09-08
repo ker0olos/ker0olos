515c5d3f994c22fb09be0a32bf62a214ee92782f
Kerolos Zaki <ker0olos@pm.me>
2021-09-08 16:54:16 +0200

    fixed picture-in-picture on dual monitors

--- .bin/external_rule_pip.sh
+++ .bin/external_rule_pip.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 wid="$1"
 class="$2"
@@ -6,5 +6,12 @@ instance="$3"
 
 title="$(xtitle "$wid")"
 
-[ -z "$class" ] && [ -z "$instance" ] && [ "$title" = "Picture in picture" ] \
-    && echo 'state=floating rectangle=520x240+780+450'
+external_monitor_1=$(xrandr --query | grep 'VGA-1')
+
+if [ -z "$class" ] && [ -z "$instance" ] && [ "$title" = "Picture in picture" ]; then
+	if [[ "$external_monitor_1" == "VGA-1 connected"* ]]; then
+		echo 'state=floating rectangle=520x240+1840+450'
+	else
+		echo 'state=floating rectangle=520x240+780+450'
+	fi
+fi
