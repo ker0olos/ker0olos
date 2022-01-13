6fa772385dc79a322d5a1e9e4737362afae136fa
Kerolos Zaki <ker0olos@pm.me>
2022-01-13 23:40:34 +0200

    defaulted my pc to use the nvidia gpu

--- .bin/start-chrome.sh
+++ .bin/start-chrome.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-google-chrome-stable $1 --enable-features=WebUIDarkMode --force-dark-mode --disk-cache-dir="$XDG_RUNTIME_DIR/chromium-cache"
+google-chrome-stable $1 --enable-features=WebUIDarkMode --force-dark-mode
