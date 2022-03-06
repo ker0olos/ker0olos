e7bbd0c12122a9acaa3626f1427cd306b9738c4d
Kerolos Zaki <ker0olos@pm.me>
2022-03-06 07:24:30 +0200

    fix pip external rule

--- .bin/external_rule_pip.sh
+++ .bin/external_rule_pip.sh
@@ -6,10 +6,10 @@ instance="$3"
 
 title="$(xtitle "$wid")"
 
-external_monitor_1=$(xrandr --query | grep 'VGA-1')
+external_monitor_1=$(xrandr --query | grep 'VGA-1-1')
 
 if [ -z "$class" ] && [ -z "$instance" ] && [ "$title" = "Picture in picture" ]; then
-	if [[ "$external_monitor_1" == "VGA-1 connected"* ]]; then
+	if [[ "$external_monitor_1" == "VGA-1-1 connected"* ]]; then
 		echo 'state=floating rectangle=520x240+1840+450'
 	else
 		echo 'state=floating rectangle=520x240+780+450'
