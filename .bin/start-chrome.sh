e82a5425eaf8edfb7590cf34aff9fdc20fd22898
Kerolos Zaki <ker0olos@pm.me>
2022-03-06 07:25:16 +0200

    add chrome incognito shortcut

--- .bin/start-chrome.sh
+++ .bin/start-chrome.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-google-chrome-stable $1 --enable-features=WebUIDarkMode --force-dark-mode
+if [[ "$1" == "--incognito" ]]; then
+  google-chrome-stable $2 --enable-features=WebUIDarkMode --force-dark-mode --incognito
+else
+  google-chrome-stable $1 --enable-features=WebUIDarkMode --force-dark-mode
+fi
