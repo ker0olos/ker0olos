fa542faa8a179041222188ca94a69b7369cbb1a1
Kerolos Zaki <ker0olos@pm.me>
2022-03-30 02:23:33 +0200

    pseudo tile windows named `cv2`

--- .bin/external_rule_pip.sh
+++ .bin/external_rule_pip.sh
@@ -8,10 +8,17 @@ title="$(xtitle "${wid}")"
 
 external_monitor_1=$(xrandr --query | grep 'VGA-1-1')
 
-if [ -z "${class}" ] && [ -z "${instance}" ] && [ "${title}" = "Picture in picture" ]; then
-	if [[ ${external_monitor_1} == "VGA-1-1 connected"* ]]; then
-		echo 'state=floating rectangle=520x240+1840+450'
-	else
-		echo 'state=floating rectangle=520x240+780+450'
-	fi
+# if no window class is defined
+if [ -z "${class}" ] && [ -z "${instance}" ]; then
+	case "${title}" in
+	"Picture in picture")
+		if [[ ${external_monitor_1} == "VGA-1-1 connected"* ]]; then
+			echo 'state=floating rectangle=520x240+1840+450'
+		else
+			echo 'state=floating rectangle=520x240+780+450'
+		fi
+		;;
+	"cv2") echo 'state=pseudo_tiled' ;;
+	*) echo 'state=tiled' ;;
+	esac
 fi
