8e808719f8549691ba24315fd6d56666cd1c51f1
Kerolos Zaki <ker0olos@pm.me>
2022-09-05 07:07:49 +0200

    spt.py: fixed some syntax issues

--- .bin/spt.py
+++ .bin/spt.py
@@ -8,8 +8,6 @@ import datetime
 
 import spotipy
 
-command = sys.argv[1] if len(sys.argv) > 1 else sys.exit()
-
 curr_name = "Space"
 curr_date = datetime.date.today().strftime("%d/%m/%Y")
 cache_path = os.environ["HOME"] + "/sptpy_cache.json"
@@ -55,6 +53,8 @@ def random_stat():
 
 
 if __name__ == "__main__":
+    command = sys.argv[1] if len(sys.argv) > 1 else sys.exit()
+
     try:
         spotify = spotipy.Spotify(
             auth_manager=spotipy.oauth2.SpotifyOAuth(
@@ -86,56 +86,58 @@ if __name__ == "__main__":
         sys.exit()
 
     if currently_playing is None:
-        if command == "title":
-            print(random_stat())
-        elif command == "toggle":
-            print("Getting recently played tracks")
-            recently_played = spotify.current_user_recently_played(limit=50)
-            recently_played_uris = list(
-                map(lambda v: v["track"]["uri"], recently_played["items"])
-            )
+        match command:
+            case "title":
+                print(random_stat())
+            case "toggle":
+                print("Getting recently played tracks")
+                recently_played = spotify.current_user_recently_played(limit=50)
+                recently_played_uris = list(
+                    map(lambda i: i["track"]["uri"], recently_played["items"])
+                )
 
-            print(f"Looking for {curr_name}")
-            devices = spotify.devices()["devices"]
-            space_id = None
+                print(f"Looking for {curr_name}")
+                devices = spotify.devices()["devices"]
+                space_id = None
 
-            for d in devices:
-                if d["name"] == curr_name:
-                    space_id = d["id"]
-                    break
+                for d in devices:
+                    if d["name"] == curr_name:
+                        space_id = d["id"]
+                        break
 
-            if space_id is None:
-                print(f"{curr_name} not found")
-                sys.exit()
+                if space_id is None:
+                    print(f"{curr_name} not found")
+                    sys.exit()
 
-            spotify.start_playback(space_id, uris=recently_played_uris)
-            spotify.repeat("context", space_id)
-            spotify.shuffle(True, space_id)
-        else:
-            print("")
+                spotify.start_playback(space_id, uris=recently_played_uris)
+                spotify.repeat("context", space_id)
+                spotify.shuffle(True, space_id)
+            case _:
+                print("")
     # something is currently playing
     else:
         if currently_playing["item"] is not None:
             tid = currently_playing["item"]["id"]
 
-            if command == "title":
-                print(currently_playing["item"]["name"])
-            elif command == "skip":
-                spotify.next_track()
-            elif command == "toggle" and currently_playing["is_playing"]:
-                spotify.pause_playback()
-            elif command == "toggle":
-                spotify.start_playback()
-            elif command == "state":
-                print("󰋑") if spotify.current_user_saved_tracks_contains(tracks=[tid])[
-                    0
-                ] else print("󰋕")
-            elif command == "like":
-                if not spotify.current_user_saved_tracks_contains(tracks=[tid])[0]:
-                    spotify.current_user_saved_tracks_add(tracks=[tid])
-                else:
-                    spotify.current_user_saved_tracks_delete(tracks=[tid])
+            match command:
+                case "title":
+                    print(currently_playing["item"]["name"])
+                case "skip":
+                    spotify.next_track()
+                case "toggle":
+                    spotify.pause_playback() if currently_playing[
+                        "is_playing"
+                    ] else spotify.start_playback()
+                case "state":
+                    print("󰋑") if spotify.current_user_saved_tracks_contains(
+                        tracks=[tid]
+                    )[0] else print("󰋕")
+                case "like":
+                    if not spotify.current_user_saved_tracks_contains(tracks=[tid])[0]:
+                        spotify.current_user_saved_tracks_add(tracks=[tid])
+                    else:
+                        spotify.current_user_saved_tracks_delete(tracks=[tid])
         elif command == "title":
-            print("Unknown Track")
+            print("Unsupported media type")
         else:
             print("")
