3f56d8db56b6f5c0dd30b7903283612c814f66e1
Kerolos Zaki <ker0olos@pm.me>
2022-09-21 20:51:33 +0200

    reenable the workspace polybar

--- .bin/workspace.sh
+++ .bin/workspace.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-WID="$(bspc query -N -n .local.hidden)"
+# WID="$(bspc query -N -n .local.hidden)"
 WORKSPACE="$(bspc query -D -d focused --names)"
 
-if [ -z $WID ]; then
-  echo "$WORKSPACE"
-else
-  echo "$WORKSPACE" "$(xprop -id $WID | grep WM_CLASS | awk '{print $4}' | sed -E 's/(^.)|(.$)//g')"
-fi
+# if [ -z $WID ]; then
+#   echo "$WORKSPACE"
+# else
+#   echo "$WORKSPACE" "$(xprop -id $WID | grep WM_CLASS | awk '{print $4}' | sed -E 's/(^.)|(.$)//g')"
+# fi
+
+echo "$WORKSPACE"
