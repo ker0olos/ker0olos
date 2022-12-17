9ff6a0dfee2141a8d004e74b3d929f84ac985226
Kerolos Zaki <ker0olos@pm.me>
2022-12-17 12:49:26 +0200

    update the chrome flags

--- .bin/start-chrome.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ $1 == "--incognito" ]]; then
-	google-chrome-stable "$2" --enable-features=WebUIDarkMode --force-dark-mode --password-store=gnome --incognito
-else
-	google-chrome-stable "$1" --enable-features=WebUIDarkMode --force-dark-mode --password-store=gnome
-fi
